983c4ba6caaa31dff7a84ca3a50b1c85e0fd1e37
Alexia Leplus <alexia.leplus@epitech.eu>
2017-09-28 15:17:27 +0200

    Vues créées

--- cards/src/AppBundle/Controller/DefaultController.php
+++ cards/src/AppBundle/Controller/DefaultController.php
@@ -5,15 +5,27 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
 
-        return $this->render('home/index.html.twig');
+        return $this->render('AppBundle:home:index.html.twig');
+    }
+
+    /**
+     * @Route("/new", name="call")
+     */
+    public function call() {
+
+        dump(file_get_contents('https://recrutement.local-trust.com/test/cards/57187b7c975adeb8520a283c'));
+
+        return $this->render('AppBundle:home:show.html.twig');
     }
 }
